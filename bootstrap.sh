cebfeea7548f2a8ef6c070eb7dfdeca0e4e30049
rammiah <rammiahcn@gmail.com>
2020-03-26 15:21:32 +0800

    add couchdb support

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker pull hyperledger/fabric-peer:2.0.0
+docker pull hyperledger/fabric-orderer:2.0.0
+docker pull hyperledger/fabric-tools:2.0
+docker pull hyperledger/fabric-baseos:2.0.0
+docker pull couchdb:2.3
--- start-network.sh
+++ start-network.sh
@@ -4,5 +4,5 @@
 ./generate.sh
 
 echo "-----starting orderers and peers-----"
-docker-compose -f docker-compose-cli.yaml -f docker-compose-etcdraft2.yaml up -d
+docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml  -f docker-compose-etcdraft2.yaml up -d
 
