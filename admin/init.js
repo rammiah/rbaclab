dc217fc8fa1501e31c3d966a15295872f8ea92c8
rammiah <rammiahcn@gmail.com>
2020-04-12 15:00:06 +0800

    add rbac content

--- admin/init.js
+++ admin/init.js
@@ -4,7 +4,38 @@ const {Wallets, Gateway} = require('fabric-network');
 const fs = require('fs');
 const yaml = require('js-yaml');
 const path = require('path');
-const {File, Permission} = require('./entity.js');
+const {File} = require('./entity.js');
+
+async function addFiles(contract, files) {
+    try {
+        // console.log(JSON.stringify(files));
+        for (let file of files) {
+            console.log(JSON.stringify(file))
+            await contract.submitTransaction('addFile', JSON.stringify(file));
+        }
+    } catch (err) {
+        console.log(`addFiles error: ${err.message}`);
+    }
+}
+
+async function addPermissions(contract, permissions) {
+    for (let permission of permissions) {
+        await contract.submitTransaction('addPermission', JSON.stringify(permission));
+    }
+}
+
+async function addRoles(contract, roles) { 
+    for (let role of roles) {
+        await contract.submitTransaction('addRole', JSON.stringify(role));
+    }
+
+}
+
+async function addUsers(contract, users) {
+    for (let user of users) {
+        await contract.submitTransaction('addUser', JSON.stringify(user));
+    }
+}
 
 async function main() {
     const gateway = new Gateway();
@@ -21,12 +52,18 @@ async function main() {
         await gateway.connect(connProfile, connOpt);
         const channel = await gateway.getNetwork('rbacchannel');
         const contract = channel.getContract('rbac', 'org.rammiah.rbac');
+
         console.log('add file');
-        const permission = new Permission('read-a');
-        const file = new File('file-a', permission);
+        const file = new File('file-a', 'permission-a');
         console.log(JSON.stringify(file));
         const resp = await contract.submitTransaction('addFile', JSON.stringify(file));
-        console.log(`add file response : ${resp}`);
+        console.log(`add file response: ${resp}`);
+        // const js = JSON.parse(fs.readFileSync('./rbac.json', 'utf8'));
+        // console.log(JSON.stringify(js, null, 2));
+        // await addFiles(contract, js['files']);
+        // await addPermissions(contract, js['permissions']);
+        // await addRoles(contract, js["roles"]);
+        // await addUsers(contract, js["users"]);
     } catch (err) {
         console.log(`add file error: ${err}`)
     } finally {
